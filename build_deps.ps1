a25235f827d7ef96210c97dd67ab044c5e0b7063
Bruno S Marques <brunosmarques1995@hotmail.com>
2024-04-27 21:59:57 -0300

    now the route is sending emails

--- build_deps.ps1
+++ build_deps.ps1
@@ -4,6 +4,15 @@ param (
 
 if (($buildMode -eq "Debug" -or $buildMode -eq "Release"))
 {
+    git clone --recursive https://github.com/openssl/openssl.git ./modules/openssl
+    $install_prefix= Join-Path (Get-Location) "/Windows/$buildMode"
+    Set-Location ./modules/openssl
+    Write-Host "$install_prefix"
+    Start-Process vcvars64.bat
+    perl ./Configure VC-WIN64A --prefix="$install_prefix" --openssldir="$install_prefix/openssl" -shared
+    nmake
+    nmake install
+    Set-Location ../..
     git clone --recursive https://github.com/madler/zlib.git ./modules/zlib
     cmake -S ./modules/zlib -B ./dependencies/zlib -DCMAKE_INSTALL_PREFIX="./Windows/$buildMode"
     cmake --build ./dependencies/zlib --config "$buildMode" --target install
@@ -13,6 +22,12 @@ if (($buildMode -eq "Debug" -or $buildMode -eq "Release"))
     git clone --recursive https://github.com/drogonframework/drogon.git ./modules/drogon
     cmake -S ./modules/drogon -B ./dependencies/drogon -DCMAKE_INSTALL_PREFIX="./Windows/$buildMode" -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=OFF -DBUILD_CTL=OFF
     cmake --build ./dependencies/drogon --config "$buildMode" --target install
+    git clone --recursive https://github.com/boostorg/boost.git ./modules/boost -DBUILD_SHARED_LIBS=ON
+    cmake -S ./modules/boost -B ./dependencies/boost -DCMAKE_INSTALL_PREFIX="./Windows/$buildMode"
+    cmake --build ./dependencies/boost --config "$buildMode" --target install
+    git clone --recursive https://github.com/karastojko/mailio.git ./modules/mailio 
+    cmake -S ./modules/mailio -B ./dependencies/mailio -DCMAKE_INSTALL_PREFIX="./Windows/$buildMode" -DMAILIO_BUILD_DOCUMENTATION=OFF
+    cmake --build ./dependencies/mailio --config "$buildMode" --target install
 }
 else 
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <drogon/drogon.h>
+#include <mailio/mailboxes.hpp>
+#include <mailio/message.hpp>
+#include <mailio/smtp.hpp>
 
 int main(int argc, char** argv)
 {
@@ -9,6 +12,50 @@ int main(int argc, char** argv)
             std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
                 auto resp = drogon::HttpResponse::newHttpResponse();
                 resp->setBody("Hello, World!");
+                callback(resp);
+        },
+        { drogon::Get });
+
+    drogon::app().registerHandler(
+        "/mail",
+        [](const drogon::HttpRequestPtr&,
+            std::function<void(const drogon::HttpResponsePtr&)>&& callback) {
+                auto resp = drogon::HttpResponse::newHttpResponse();
+
+                try
+                {
+                    // create mail message
+                    mailio::message msg;
+                    msg.from(mailio::mail_address("Pairmeet Dev", "pairmeetdev@gmail.com"));// set the correct sender name and address
+                    msg.add_recipient(mailio::mail_address("B Marques", "testebash@outlook.com"));// set the correct recipent name and address
+                    msg.subject("smtps multipart message");
+                    msg.boundary("012456789@mailio.dev");
+                    msg.content_type(mailio::message::media_type_t::MULTIPART, "related");
+                    msg.content("<p>Esse veio do C++</p>");
+
+                    mailio::mime title;
+                    title.content_type(mailio::message::media_type_t::TEXT, "html", "utf-8");
+                    title.content_transfer_encoding(mailio::mime::content_transfer_encoding_t::BIT_8);
+                    title.content("<html><head></head><body><h1>Senior Call</h1></body></html>");
+
+                    msg.add_part(title);
+
+                    // connect to server
+                    mailio::smtps conn("smtp.gmail.com", 465);
+                    // modify username/password to use real credentials
+                    conn.authenticate("pairmeetdev@gmail.com", "i'm not crazy", mailio::smtps::auth_method_t::LOGIN);
+                    conn.submit(msg);
+                }
+                catch (mailio::smtp_error& exc)
+                {
+                    std::cout << exc.what() << std::endl;
+                }
+                catch (mailio::dialog_error& exc)
+                {
+                    std::cout << exc.what() << std::endl;
+                }
+
+                resp->setBody("Executed");
                 callback(resp);
         },
         { drogon::Get });
